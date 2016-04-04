1d447184eed70f45a46a791b72b51be646419d4e
Creyn <dev@creyn.pl>
2016-04-04 23:18:54 +0200

    Add very simple Aurelia website. Add CORS headers to API.

---
+++ SmartLodowka/SmartLodowkaAPI/Aurelia/src/app.js
@@ -0,0 +1,14 @@
+﻿export class App 
+{
+    configureRouter(config, router) 
+    {
+        config.title = 'Aurelia';
+        config.map([
+          { route: [''], name: 'events', moduleId: './events', nav: true, title:'Events' }
+        ]);
+
+        this.router = router;
+    }
+}
+
+  
---
+++ SmartLodowka/SmartLodowkaAPI/Aurelia/src/events.js
@@ -0,0 +1,36 @@
+﻿import {inject} from 'aurelia-framework';
+import {HttpClient} from 'aurelia-fetch-client';
+import 'fetch';
+
+@inject(HttpClient)
+export class Events 
+{
+    heading = 'Smart Lodówka Events';
+    events = [];
+
+    constructor(http) 
+    {
+        http.configure(config => {
+            config
+              .useStandardConfiguration()
+              .withBaseUrl('http://smartlodowka.creyn.pl/api/')
+              //.withBaseUrl('http://localhost:62711/api/')
+              .withDefaults({
+                  headers: {
+                      'Accept': 'application/json'
+                  }
+              })
+            ;
+        });
+
+        this.http = http;
+    }
+
+    activate() 
+    {
+        return this.http.fetch('events')
+            .then(response => response.json())
+            .then(json => this.events = json)
+    }
+}
+
